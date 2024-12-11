32896e51cbf4dedf7d6ea0cb681a3d07eb828f11
GorosX <gorosking7@gmail.com>
2024-12-11 22:28:50 +0100

    Create echo.sh

---
+++ echo.sh
@@ -0,0 +1,16 @@
+# Ejemplo 1: Mostrar un mensaje simple
+echo "Hola, esto es un mensaje simple"
+
+# Ejemplo 2: Mostrar el contenido de una variable
+mensaje="Hola desde una variable"
+echo $mensaje
+
+# Ejemplo 3: Usar caracteres especiales con `-e`
+echo -e "Línea 1\nLínea 2\nLínea 3"
+
+# Ejemplo 4: No incluir salto de línea con `-n`
+echo -n "Este mensaje no tiene salto de línea"
+echo " - Continuación del mensaje"
+
+# Ejemplo 5: Mostrar un mensaje con comillas
+echo '"Este mensaje está entre comillas dobles"'
