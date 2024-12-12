6616ac19909e0d36155991fd66188cd75a65b317
GorosX <gorosking7@gmail.com>
2024-12-12 09:46:05 +0100

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -9,3 +9,13 @@ if (( year % 4 == 0 && year % 100 != 0 )) || (( year % 400 == 0 )); then
 else
     echo "El año $year no es bisiesto."
 fi
+
+# Corregido cepis mode
+# Pedimos al usuario un año
+read -p "Introduce un año: " year
+ 
+if [ $((year % 4)) -eq 0 ] && [ $((year % 100)) -ne 0 ] || [ $((year % 400)) -eq 0 ]; then
+    echo "El año $year es bisiesto."
+else
+    echo "El año $year no es bisiesto."
+fi
